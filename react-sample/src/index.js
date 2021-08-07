6bb024c4f70457fb8f8808178aee2b1a9526d1fe
minty1202 <aki.men1202@gmail.com>
2021-08-07 19:32:33 +0900

    Boardに保存するように追記（おそらく）

--- react-sample/src/index.js
+++ react-sample/src/index.js
@@ -14,7 +14,7 @@ class Square extends React.Component {
     return (
       <button
       className="square"
-      onClick={()=> this.setState({value: 'X'})}
+      onClick={() => this.setState({value: 'X'})}
       >
       { this.state.value }
       </button>
@@ -23,8 +23,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+      value={ this.state.squares[i] }
+      onClick={ () => this.handleClick(i) }  
+      />
+    );
   }
 
   render() {
